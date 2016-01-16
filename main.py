41f5af2bc28b1f80acf32061d282f53f2ab34baa
PolBaladas <polbaladasluna@gmail.com>
2016-01-16 14:42:10 +0100

    Initial commit. add map and form

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
